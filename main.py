d1458c304a47cc2ebe31ad1ee9078ccb171081b3
kesslernd <kesslernd@gmail.com>
2024-02-03 16:25:38 -0700

    use os

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 from YoutubeSearchClient import YoutubeSearchClient
 from YoutubeSearchBot import YoutubeSearchBot
+import os
 
-DISCORD_TOKEN = '<DISCORD TOKEN>'
-YT_API_KEY = '<YOUTUBE API KEY>'
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+YT_TOKEN = os.getenv('YT_TOKEN')
 
 load_dotenv()
-bot = YoutubeSearchBot(yt_client=YoutubeSearchClient(YT_API_KEY))
+bot = YoutubeSearchBot(yt_client=YoutubeSearchClient(YT_TOKEN))
 bot.run(DISCORD_TOKEN)
